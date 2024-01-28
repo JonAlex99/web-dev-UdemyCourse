cca004a49b4a3bbd5de57c4035f68dc3e7d64013
Jón Alex <jonalexpetursson@gmail.com>
2024-01-28 21:10:58 +0000

    eitthvað weird commit

--- Section_23/2.3 NPM/index.js
+++
@@ -1,9 +0,0 @@
-import generateName from "sillyname";
-import generateSuperHero from "superheroes";
-var sillyName = generateName();
-
-console.log(`My name is ${sillyName}.`);
-
-var superHero = generateSuperHero.random();
-
-console.log(`No need to fear, ${superHero} is here`)
